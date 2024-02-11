0a4af80da3c144c8fd6e4b506177c8908f7566ee
irfan-fauzi <irfanwintech@yahoo.com>
2024-02-11 22:43:55 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import session from "express-session";
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173"],
+  })
+);
+
+interface SessionConfigInterface {
+  secret: any;
+  resave: boolean;
+  saveUninitialized: boolean;
+  cookie: object;
+}
+
+const sessionConfig: SessionConfigInterface = {
+  secret: process.env.SESS_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: "auto", // secure: true = http || secure: false = https
+  },
+};
+
+app.use(session(sessionConfig));
+
+app.use(express.json());
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send({
+    msg: "🧑 🐱 💟 ",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`app running in port: ${PORT}`);
+});
