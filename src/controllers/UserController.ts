2194237fbcccf99f2dbc84eacaeceddb8aecb9cc
irfan-fauzi <irfanwintech@yahoo.com>
2024-02-16 16:43:49 +0700

    hashing password

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,14 +1,17 @@
 import { type Request, type Response } from "express";
 import User from "../../models/User";
 import Helper from "../helper/Helper";
+import PasswordHelper from "../helper/PasswordHelper";
 
 export const Register = async (req: Request, res: Response) => {
   try {
     const { name, email, password, confirmPassword } = req.body;
+    const hashPassword = await PasswordHelper.PasswordHashing(password);
+
     const user = await User.create({
       name,
       email,
-      password,
+      password: hashPassword,
       active: true,
       verified: true,
       roleId: 1,
